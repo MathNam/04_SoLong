69cc22a178b9786175456bf5eac2214a0fbdf315
Mathieu ALIBERT <maaliber@e1r8p11.clusters.42paris.fr>
2023-04-17 17:39:54 +0200

    move_cnt Terminal

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: maaliber <maaliber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:28:27 by maaliber          #+#    #+#             */
-/*   Updated: 2023/03/22 14:02:07 by maaliber         ###   ########.fr       */
+/*   Updated: 2023/04/17 17:39:08 by maaliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	move_up(t_game *data)
 		if (ft_strchr("cEI", data->map[p1->y][p1->x].type))
 			p1->action = 0;
 		p1->y = p1->y - 1;
-		data->mv_cnt++;
+		ft_printf("%d\n", ++(data->mv_cnt));
 		if (data->map[p1->y][p1->x].type == 'C')
 		{
 			data->map[p1->y][p1->x].type = 'c';
@@ -49,7 +49,7 @@ void	move_down(t_game *data)
 		if (ft_strchr("cEI", data->map[p1->y][p1->x].type))
 			p1->action = 0;
 		p1->y = p1->y + 1;
-		data->mv_cnt++;
+		ft_printf("%d\n", ++(data->mv_cnt));
 		if (data->map[p1->y][p1->x].type == 'C')
 		{
 			data->map[p1->y][p1->x].type = 'c';
@@ -75,7 +75,7 @@ void	move_left(t_game *data)
 		if (ft_strchr("cEI", data->map[p1->y][p1->x].type))
 			p1->action = 0;
 		p1->x = p1->x - 1;
-		data->mv_cnt++;
+		ft_printf("%d\n", ++(data->mv_cnt));
 		if (data->map[p1->y][p1->x].type == 'C')
 		{
 			data->map[p1->y][p1->x].type = 'c';
@@ -101,7 +101,7 @@ void	move_right(t_game *data)
 		if (ft_strchr("cEI", data->map[p1->y][p1->x].type))
 			p1->action = 0;
 		p1->x = p1->x + 1;
-		data->mv_cnt++;
+		ft_printf("%d\n", ++(data->mv_cnt));
 		if (data->map[p1->y][p1->x].type == 'C')
 		{
 			data->map[p1->y][p1->x].type = 'c';
